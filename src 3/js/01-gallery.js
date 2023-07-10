3ba794afb8f80195eae7969b04136a6b5c78859f
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2023-07-10 16:33:22 +0300

    Connecting to the script and styles of the basicLightbox modal window library

--- src 3/js/01-gallery.js
+++ src 3/js/01-gallery.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallerySmallImg = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) =>
+`<li class="gallery__item">
+  <a class="gallery__link" href="${preview}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li` ).join('');
+
+gallerySmallImg.insertAdjacentHTML('beforeend', markup);
+
+gallerySmallImg.addEventListener('click', onClick);
+
+function onClick (evt) {
+  evt.preventDefault();
+  console.log(evt.target);
+  console.log(evt.currentTarget);
+}
+
